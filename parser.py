f4e8158d2d2999e50468e6e18bc0915cadbd5842
varunrau <varunrau@gmail.com>
2013-09-12 21:04:13 -0700

    some of right works

--- parser.py
+++ parser.py
@@ -8,17 +8,122 @@ class Face():
 		self.matrix = np.reshape(listrep,(3,3))
 		print self.matrix
 
-class Cube()
+	def isSolved(self):
+		mList = self.matrix.tolist()
+		firstElem = mList[0][0]
+		for l in mList:
+			if l != firstElem:
+				return False
+		return True
+
+
+class Cube():
 
 	def __init__(self, faces):
 		self.faces = faces
+		self.right()
 
-	def right(dir=False):
+	def right(self, dir=False):
 		print "implement"
 		if dir:
-			print "prime"
+
+			top = self.faces[0].matrix
+			front = self.faces[2].matrix
+			back = self.faces[4].matrix
+			bottom = self.faces[5].matrix
+			right = self.faces[3].matrix
+			topTranspose = top.transpose()
+			topTemp = top
+			frontTrans = front.transpose()
+			topTransposeList = topTranspose.tolist()
+			frontTransposeList = frontTrans.tolist()
+			topTransposeList[2] = frontTransposeList[2]
+			self.faces[0].matrix = np.reshape(topTransposeList, (3, 3)).transpose()
+
+			b1, b2, b3 = back[0, 2], back[1, 2], back[2, 2]
+
+			top[2, 0] = back[0, 2]
+			top[1, 0] = back[1, 2]
+			top[0, 0] = back[2, 2]
+
+			bottom[0, 2] = front[0, 2]
+			bottom[1, 2] = front[1, 2]
+			bottom[2, 2] = front[2, 2]
+
+
+			bottom[0, 2] = b1
+			bottom[1, 2] = b2
+			bottom[2, 2] = b3
+
+			right = np.rot90(right)
+			right = np.rot90(right)
+			right = np.rot90(right)
+
+			self.faces[0].matrix = top
+			self.faces[2].matrix = front
+			self.faces[4].matrix = back
+			self.faces[5].matrix = bottom
+			self.faces[3].matrix = right
 		else:
-			top = self.faces[0]
+
+			top = self.faces[0].matrix
+			front = self.faces[2].matrix
+			back = self.faces[4].matrix
+			bottom = self.faces[5].matrix
+			right = self.faces[3].matrix
+			topTranspose = top.transpose()
+			topTemp = top
+			frontTrans = front.transpose()
+			topTransposeList = topTranspose.tolist()
+			frontTransposeList = frontTrans.tolist()
+			topTransposeList[2] = frontTransposeList[2]
+			self.faces[0].matrix = np.reshape(topTransposeList, (3, 3)).transpose()
+
+			b1, b2, b3 = back[0, 2], back[1, 2], back[2, 2]
+
+			back[0, 2] = top[2, 0]
+			back[1, 2] = top[1, 0]
+			back[2, 2] = top[0 , 0]
+
+			front[0, 2] = bottom[0, 2]
+			front[1, 2] = bottom[1, 2]
+			front[2, 2] = bottom[2, 2]
+
+			bottom[0, 2] = b1
+			bottom[1, 2] = b2
+			bottom[2, 2] = b3
+
+			right = np.rot90(right)
+
+			self.faces[0].matrix = top
+			self.faces[2].matrix = front
+			self.faces[4].matrix = back
+			self.faces[5].matrix = bottom
+			self.faces[3].matrix = right
+
+
+
+
+
+
+
+#			for t, topRow in enumerate(top):
+#				for f, frontRow in enumerate(front):
+#					if t != f:
+#						continue
+#					temp = topRow[-1]
+#					topRow[-1] = frontRow[-1]
+#					frontRow[-1] = temp
+
+
+
+
+
+	def isSolved(self):
+		for face in self.faces:
+			if not face.isSolved():
+				return False
+		return True
 
 
 
